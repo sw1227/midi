b338b276bd16cc8946abafaedbfa67e2cc66f59c
sw1227 <32032108+sw1227@users.noreply.github.com>
2018-01-08 14:39:58 +0900

    add co-occurence matrix to heatmap.js

--- visualization/heatmap.js
+++ visualization/heatmap.js
@@ -1,10 +1,17 @@
 var margin = {top: 30, right: 30, bottom: 30, left: 30};
-var width = 600 - margin.left - margin.right;
-var height = 600 - margin.top - margin.bottom;
+var width = 500 - margin.left - margin.right;
+var height = 500 - margin.top - margin.bottom;
 
 
 // 描画領域
 var svg = d3.select("body").append("svg")
+    .attr("class", "adjacent")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var svg2 = d3.select("body").append("svg")
+    .attr("class", "cooccurence")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
@@ -12,15 +19,59 @@ var svg = d3.select("body").append("svg")
 
 
 // csvファイルの読み込み
-d3.csv("csv/14canons_0.csv", type, function(error, data) {
+d3.csv("csv/aria_0.csv", type, function(error, data) {
     if (error) throw error;
 
-    // 隣接行列的なものを作成
-    matrix = adjacentMatrix(data);
+    // 隣接行列的なもの
+    matrix1 = adjacentMatrix(data);
+    // 共起行列的なもの
+    matrix2 = cooccurenceMatrix(data);
 
     // 行列をもとにヒートマップをプロット
-    drawHeatmap(matrix);
+    drawHeatmap(matrix1, svg);
+    drawHeatmap(matrix2, svg2);
+
+    // 共起行列は対称・対角成分0なので間引き
+    d3.select("svg.cooccurence").selectAll("g.raw").each(function(g, j) {
+	d3.select(this).selectAll("rect")
+	    .filter(function(d, i) { return i >= j; })
+	    .remove();
+    });
 });
+
+
+function cooccurenceMatrix(data) {
+    // 行列の大きさを計算
+    var maxNoteNum = d3.max(data, function(d) { return d.note_num; });
+    var minNoteNum = d3.min(data, function(d) { return d.note_num; });
+    var noteRange = maxNoteNum - minNoteNum + 1;
+
+    // 行列を0で初期化
+    var matrix = new Array(noteRange);
+    for (var i=0; i<matrix.length; i++){
+	matrix[i] = new Array(noteRange).fill(0);
+    }
+
+    // データに基づき行列に値をセット
+    var startSet = d3.set(data.map(function(d) { return d.start; }));
+    var startTimes = startSet.values().map(function(d){ return +d; }).sort(d3.ascending);
+
+    startTimes.forEach(function(time) {
+	var starting = data.filter(function(d) { return d.start == time; });
+	var playing = data.filter(function(d) { return d.start <= time && time < d.end; });
+	starting.forEach(function(s) {
+	    playing.forEach(function(p) {
+		if (s.note_num != p.note_num) {
+		    // 対称にする
+		    matrix[s.note_num - minNoteNum][p.note_num - minNoteNum] += 1;
+		    matrix[p.note_num - minNoteNum][s.note_num - minNoteNum] += 1;
+		}
+	    });
+	});
+    });
+
+    return matrix;
+}
 
 
 function adjacentMatrix(data) {
@@ -41,8 +92,8 @@ function adjacentMatrix(data) {
     var startTimes = startSet.values().map(function(d){ return +d; }).sort(d3.ascending);
 
     for (var i=0; i<startTimes.length; i++){
-	var to = data.filter(function (d) { return d.start == startTimes[i]; });
-	var from = data.filter(function (d) { return d.end == startTimes[i]; });
+	var to = data.filter(function(d) { return d.start == startTimes[i]; });
+	var from = data.filter(function(d) { return d.end == startTimes[i]; });
 	for (var t=0; t<to.length; t++){
 	    for (var f=0; f<from.length; f++){
 		matrix[ from[f].note_num - minNoteNum ][ to[t].note_num - minNoteNum ] += 1;
@@ -54,7 +105,7 @@ function adjacentMatrix(data) {
 }
 
 
-function drawHeatmap(matrix){
+function drawHeatmap(matrix, svg){
     rectSize = Math.min(width, height) / matrix.length;
     svg.selectAll(".raw")
 	.data(matrix)
